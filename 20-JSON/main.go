71ae43005740d5260d2a6acc60dad968cef5bf92
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-07 13:03:37 -0300

    Aulas 47 e 48

---
+++ 20-JSON/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type cachorro struct {
+	Nome  string `json:"nome"`
+	Raca  string `json:"raca"`
+	Idade uint   `json:"idade"`
+}
+
+func main() {
+	// json.Marshal()   //transforma um struct ou map etc em json
+	// json.Unmarshal() // transforma um json em struct ou map
+
+	c := cachorro{"Rex", "Dalmata", 3}
+	fmt.Println(c)
+
+	cachorroEmJson, erro := json.Marshal(c)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(cachorroEmJson)
+
+	fmt.Println(bytes.NewBuffer(cachorroEmJson))
+
+	c2 := map[string]string{
+		"nome": "Toby",
+		"raca": "Poodle",
+	}
+
+	cachorro2EmJson, erro := json.Marshal(c2)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(cachorro2EmJson)
+
+	fmt.Println(bytes.NewBuffer(cachorro2EmJson))
+}
