d51ad87621fb105a0e42a64f103ffdbb8c187b71
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-08 13:11:47 -0300

    Até a aula 50

---
+++ 21-http/http.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá mundo!"))
+}
+
+func usuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Carregar pagina de usuarios"))
+}
+
+func main() {
+	// HTTP É UM PROTOCOLO DE COMUNICAÇÃO - BASE DA COMUNICAÇÃO WEB
+
+	// CLIENTE (FAZ REQUISIÇÃO) - SERVIDOR (PROCESSA REQUISIÇÃO E ENVIA RESPOSTA)
+
+	// REQUEST - RESPONSE
+
+	// ROTAS
+	// URI - Identificador do Recurso (/user, /produtos)
+	// METODO - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/home", home)
+
+	http.HandleFunc("/usuarios", usuarios)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
